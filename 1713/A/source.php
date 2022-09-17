0a4b749ca49808d7ff381c318b18abfef6a2b26a
rela589n <zsidelnik@gmail.com>
2022-09-17 09:33:00 +0300

    1713 A. Traveling Salesman Problem

---
+++ 1713/A/source.php
@@ -0,0 +1,25 @@
+<?php
+
+fscanf(STDIN, '%u', $t);
+
+while ($t--) {
+    fscanf(STDIN, '%u', $n);
+
+    $minX = 0;
+    $maxX = 0;
+    $minY = 0;
+    $maxY = 0;
+
+    for ($i = 0; $i < $n; ++$i) {
+        fscanf(STDIN, '%d %d', $x, $y);
+
+        $minX = min($minX, $x);
+        $maxX = max($maxX, $x);
+        $minY = min($minY, $y);
+        $maxY = max($maxY, $y);
+    }
+
+    $res = 2 * ($maxX + $maxY - $minX - $minY);
+
+    echo $res.PHP_EOL;
+}
